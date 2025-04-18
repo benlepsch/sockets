bf624e94aad0ff80eb5dd7bcc5a0b413103d5356
Ben Lepsch <blepsch@gmail.com>
2025-04-18 14:11:32 -0400

    start http response type

--- src/http.rs
+++ src/http.rs
@@ -57,3 +57,52 @@ impl HttpRequest {
         out_str.into_bytes()
     }
 }
+
+
+pub struct HttpResponse {
+    protocol: String,
+    status_code: u32,
+    status_msg: String,
+    headers: HashMap<String, String>,
+    body: Option<String>,
+}
+
+impl HttpResponse {
+    pub fn new(status_code: u32, status_msg: String, protocol: Option<String>) -> HttpResponse {
+        HttpResponse {
+            protocol: match protocol => {
+                Some(p) => p,
+                None => { "HTTP/1.1".to_string() },
+            },
+            status_code: status_code,
+            status_msg: status_msg,
+            headers: HashMap::new(),
+            body: None,
+        }
+    }
+
+    pub fn header(&mut self, key: String, val: String) -> &HttpResponse {
+        self.headers.insert(key, val);
+
+        self
+    }
+
+    pub fn body(&mut self, body: String) -> &HttpResponse {
+        self.body = Some(body);
+        
+        self
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        // set content-length header
+        self.headers.insert("Content-Length", match self.body {
+            Some(b) => b.len().to_string(),
+            None => "0".to_string(),
+        });
+
+        // todo: convert the headers & body to bytes
+
+        let mut out_str = format!("{} {} {}", &self.protocol, self.status_code, &self.status_msg);
+        out_str.into_bytes()
+    }
+}
